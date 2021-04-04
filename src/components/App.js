e429657359dbdcc5854753d26681031299527363
Jarone McCorkle <60796768+jcork4real@users.noreply.github.com>
2021-04-04 15:47:04 -0600

    Add files via upload
    added hooks and json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
+
+import React, {useState, useEffect} from "react";
+import { isNull } from 'lodash';
 import '../css/App.css';
+import AddAppointments from  './AddAppointments';
+import ListAppointments from  './ListAppointments';
+import SearchAppointments from  './SearchAppointments';
+
 
 function App() {
+
+
+  const[appointments, setAppointments] = useState(null);
+
+  useEffect(()=> {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(setAppointments);
+    
+  }, []);
+  
   return (
     <main className="page bg-white" id="petratings">
     <div className="container">
       <div className="row">
         <div className="col-md-12 bg-white">
           <div className="container">
-            <div>Add Appointments</div>
-            <div>Search Appointments</div>
-            <div>List Appointments</div>
+          <AddAppointments/>
+            <ListAppointments/>
+              <SearchAppointments/>
           </div>
         </div>
       </div>
